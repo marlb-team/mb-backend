9b71a30519c523a1ad6b46b589ae0d232302060b
Lopes Boa <lopesboa1@gmail.com>
2020-02-13 21:13:43 -0300

    Add login routes

--- src/routes/clientsRoutes.js
+++ src/routes/clientsRoutes.js
@@ -1,15 +1,14 @@
 import { Router } from 'express'
 
-import authController from '../controllers/authController'
+const clientRoute = new Router()
 
-const clientRoute = new Router();
+clientRoute.get('/clients', (req, res) => {
+  res.json({ page: 'Clientes', message: 'Página clientes' })
+})
 
-clientRoute.get("/clients", (req, res) => {
-  res.json({ page: "Clientes", message: "Página clientes" });
-});
+clientRoute.get('/api/clients', authController.index)
+clientRoute.post('/api/clients/create', authController.create)
+clientRoute.post('/api/clients/login', authController.login)
+clientRoute.get('/api/clients/login', authController.login)
 
-clientRoute.get("/api/clients", authController.index);
-clientRoute.post("/api/clients/create", authController.create);
-clientRoute.post("/api/clients/login", authController.login);
-
-export default clientRoute;
+export default clientRoute
